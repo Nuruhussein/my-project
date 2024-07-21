851bf12cb24de3906b53a95c3f42914dbf6f39c4
Nuru Hussen <weddihaji@gmail.com>
2024-07-22 00:15:08 +0300

    logout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,29 +1,32 @@
-import React from 'react';
-import axios from 'axios';
-import { useUser } from '../Context'; // Import useUser hook to access context
-import { baseurl } from '../Urls';
+import React from "react";
+import axios from "axios";
+import { useUser } from "../Context"; // Import useUser hook to access context
+import { baseurl } from "../Urls";
 
 const Logout = () => {
   const { logout } = useUser(); // Destructure logout function from context
 
   const handleLogout = async () => {
     try {
-      await axios.get(`${baseurl}/logout`, { withCredentials: true });
-      console.log('Logout successful');
+      await axios.get(`${baseurl}/auth/logout`, { withCredentials: true });
+      console.log("Logout successful");
       logout(); // Trigger logout function to update user state
-       window.location.replace("/");
+      window.location.replace("/");
     } catch (error) {
-      console.error('Logout error:', error);
+      console.error("Logout error:", error);
     }
   };
 
   return (
     <div>
-     <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-              role="menuitem"
-            onClick={handleLogout}>Logout</a>
+      <a
+        href="#"
+        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+        role="menuitem"
+        onClick={handleLogout}
+      >
+        Logout
+      </a>
     </div>
   );
 };
